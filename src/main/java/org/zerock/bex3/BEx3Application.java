3b88dec84a8fa1c62cc801a310e0c4b57326a85b
alfer91 <dasungwkd@naver.com>
2021-09-05 20:34:31 +0900

    create project

---
+++ src/main/java/org/zerock/bex3/BEx3Application.java
@@ -0,0 +1,13 @@
+package org.zerock.bex3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BEx3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BEx3Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/zerock/bex3/BEx3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.bex3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BEx3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
